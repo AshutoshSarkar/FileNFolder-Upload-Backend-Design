a4bd4c76b0ce7fe3290f9a5885a22d8053492443
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-10-16 09:13:19 +0530

    most of the backend api writing done

--- models/file.js
+++ models/file.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
+import nodemailer from "nodemailer";
+
+config();
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -17,6 +21,34 @@ const fileSchema = new mongoose.Schema({
   },
 });
 
-const File = mongoose.model("File", fileSchema);
+//post middleware to send the email
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC", doc);
+
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    //send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: "Ashutosh Sarkar",
+      to: doc.email,
+      subject: "File uploaded successfully on cloudinary",
+      html:` <h2>hii guys</h2> <p>file uploaded view here : <a href="${doc.imageUrl}"> Click Here to Watch</a></p>`,
+    });
+
+    console.log("info", info);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const File = mongoose.model("File ", fileSchema);
 
 export default File;
